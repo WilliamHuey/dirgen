4b8caf81eed69eb295d6035d8668a7204f5fc0c1
WilliamHuey <lliw1236@yahoo.com>
2016-04-12 00:11:39 -0400

    Show warnings for repeated lines on the top level done properly

--- src/validations.js
+++ src/validations.js
@@ -9,6 +9,34 @@ import message from './messages';
 
 let validator = () => {};
 
+let searchSiblings = function(searchLine, searchLineNum, lastLineNum, siblingsLines) {
+  //Check until to the very last line is reached
+  if (searchLineNum <= lastLineNum) {
+    //Log repeats in an array within a map
+    if (siblingsLines.has(searchLine.structureName)) {
+      siblingsLines.set(searchLine.structureName,
+        siblingsLines.get(searchLine.structureName).concat([searchLineNum]));
+    } else {
+      siblingsLines.set(searchLine.structureName, [searchLineNum]);
+    }
+
+    //Only display warning messages for values
+    //of arrays that greater than 1, meaning repeats
+    if (searchLineNum === lastLineNum) {
+      for (let [key, value] of siblingsLines) {
+        if(value.length >= 2) {
+          message.warn(`Line #${value[0]}: '${key}', has repeated entries on
+          line(s): ${value.slice(1)}.
+          The last repeated value line overwritten all previous entries.`);
+        }
+      }
+    }
+
+    searchSiblings(searchLine.sibling[0], searchLine.sibling[0].nameDetails.line,
+    lastLineNum, siblingsLines);
+  }
+};
+
 Object.assign(validator.prototype, {
   topLevelRepeatedLines: (firstLine, lastLineNum) => {
     //Recursively loop through all the siblings
@@ -21,46 +49,12 @@ Object.assign(validator.prototype, {
       siblingsLines = new Map();
 
     //First top level line is the only top level means search stops early
-    console.log("firstLine.sibling", firstLine.sibling);
     if (_.isNull(firstLine.sibling) ||
       firstLine.sibling.length === 0) {
       return;
     }
 
-    while (searchLineNum <= lastLineNum) {
-      // console.log("still searching");
-      // console.log("searchLineNum", searchLineNum);
-      // console.log("lastLineNum", lastLineNum);
-
-      //console.log("searchLine.structureName", searchLine.structureName);
-      // console.log("searchLine.structureName", searchLine.structureName);
-      if (siblingsLines.has(searchLine.structureName)) {
-        console.log("repeated line");
-        siblingsLines.set(searchLine.structureName,
-          siblingsLines.get(searchLine.structureName).concat([searchLineNum]));
-        console.log("siblingsLines.get(searchLine.structureName",  siblingsLines.get(searchLine.structureName));
-      } else {
-        siblingsLines.set(searchLine.structureName, [searchLineNum]);
-      }
-
-      //Only display warning messages for values
-      //of arrays that greater than 1, meaning repeats
-      if (searchLineNum === lastLineNum) {
-        for (let [key, value] of siblingsLines) {
-          console.log(key + " = " + value);
-        }
-      }
-
-      searchLine = searchLine.sibling[0];
-      searchLineNum = searchLine.nameDetails.line;
-      // console.log("later searchLine", searchLine);
-
-
-    }
-
-    //throw new Error('asdgsdgsdfgdsg');
-    return 1;
-
+    searchSiblings(searchLine, searchLineNum, lastLineNum, siblingsLines);
   },
   repeatedLines: (lineNum, children) => {
     let childStructureNames = new Map();
@@ -71,7 +65,6 @@ Object.assign(validator.prototype, {
       let childLineNum = val.nameDetails.line;
       //Push to the array of collected repeats
       if (childStructureNames.has(val.structureName)) {
-        //TODO: set proper key and value for the map
         childStructureNames.set(val.structureName,
           childStructureNames.get(val.structureName).push(childLineNum));
         let repeatedEntries = childStructureNames.get(val.structureName);
@@ -118,18 +111,15 @@ Object.assign(validator.prototype, {
     if (indentType === 'outdent' &&
     !(currentIndentAmt % firstIndentAmt === 0) &&
     !(currentIndentAmt >= firstIndentAmt)) {
-      // console.log(`Line num: ${lineNum}` + "outdented string");
       throw (message.error(`Line #${lineNum}:
          '${content.trim()}', has indent amount of ${currentIndentAmt} ${currentIndentType}(s) which is inconsistent with the
          first defined outdent of ${firstIndentAmt} ${firstIndentType}(s).`));
     }
-    // console.log("lineNum", lineNum);
-    // console.log("now currentIndentAmt", currentIndentAmt);
+
     //Scaling indent factor and firstIndent is the same
     if (indentType !== 'outdent' && !(Math.abs(currentIndentAmt - prevLineIndentAmt) ===
        firstIndentAmt)) {
-      // console.log("on line", lineNum);
-      // console.log("not allowed indenting factor");
+
       throw (message.error(`Line #${lineNum}:
          '${content.trim()}', has an indent
          amount of ${Math.abs(currentIndentAmt - prevLineIndentAmt)} ${currentIndentType}(s) relative to parent folder,
