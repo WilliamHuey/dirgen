386c69b7ed5b45f4620d89d5dedbfa9216b81398
WilliamHuey <lliw1236@yahoo.com>
2016-04-18 00:48:56 -0400

    Use es2015 for cli indirectly

--- bin/dirgen.js
+++ bin/dirgen.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
 console.log("dirgen global commmand line execution");
+
+require('babel-core/register')({
+   presets: [ 'es2015' ]
+});
+
+require('../lib/dirgen').default();
