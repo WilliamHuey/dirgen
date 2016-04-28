a0427cd5c4a98c550c632a46ad2ca5dd8ae6a319
WilliamHuey <lliw1236@yahoo.com>
2016-04-28 00:15:16 -0400

    Use pretty error for the cli commands

--- src/lines-validations.js
+++ src/lines-validations.js
@@ -6,7 +6,7 @@ import sanitize from 'sanitize-filename';
 import recursive from 'tail-call/core';
 
 //Source modules
-import message from './lines-validations-messages';
+import message from './validations-messages';
 import structureMarker from './character-code';
 
 const tailCall = recursive.recur;
