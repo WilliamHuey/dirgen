45b6d2e99728b40bd99226ffb6c603a6f1ff06bb
WilliamHuey <lliw1236@yahoo.com>
2016-05-07 13:33:40 -0400

    Formatting: remove empty lines in the test cases

--- tests/integrity.js
+++ tests/integrity.js
@@ -5,8 +5,8 @@ var childProcess = require('child_process'),
   path = require('path');
 
 //Vendor modules
-var fs = require('fs-extra-promise');
-var lab = exports.lab = require('lab').script(),
+var fs = require('fs-extra-promise'),
+  lab = exports.lab = require('lab').script(),
   __ = require('hamjest');
 
 //Path definitions
@@ -16,11 +16,9 @@ lab.experiment('Cli commands when input is "dirgen" and', function() {
 
   var exec = childProcess.exec;
 
-  lab.experiment.skip('with no commands or options', function() {
+  lab.experiment('with no commands or options', function() {
     lab.test('will display the help message', function(done) {
-
       exec(cliEntryFile, function(error, stdout, stderr) {
-
         __.assertThat(stdout, __.containsString('Description'));
         done(error);
       });
@@ -28,88 +26,79 @@ lab.experiment('Cli commands when input is "dirgen" and', function() {
   });
 
 
-  lab.experiment.skip('and with the generate command', function() {
+  lab.experiment('and with the generate command', function() {
 
     lab.test('and no arguments displays an error message', function(done) {
-
       exec(cliEntryFile + ' generate', function(error, stdout, stderr) {
-
-        __.assertThat(stdout, __.containsString('No file template nor folder destination given.'));
+        __.assertThat(stdout,
+          __.containsString('No file template nor folder destination given.'));
         done(error);
       });
     });
 
-    lab.test('with generate command, and with no arguments will display the error message', function(done) {
-
+    lab.test('with generate command and no arguments, will display the error message', function(done) {
       exec(cliEntryFile + ' generate', function(error, stdout, stderr) {
-
-        __.assertThat(stdout, __.containsString('No file template nor folder destination given.'));
+        __.assertThat(stdout,
+          __.containsString('No file template nor folder destination given.'));
         done(error);
       });
     });
 
-    lab.test('using alias command, "g", and with no arguments will display an error message', function(done) {
-
+    lab.test('using alias command, "g", and no arguments, will display the error message', function(done) {
       exec(cliEntryFile + ' g', function(error, stdout, stderr) {
-
-        __.assertThat(stdout, __.containsString('No file template nor folder destination given.'));
+        __.assertThat(stdout,
+          __.containsString('No file template nor folder destination given.'));
         done(error);
       });
     });
 
     lab.test('using alias command, "gen", and with no arguments will display an error message', function(done) {
-
       exec(cliEntryFile + ' gen', function(error, stdout, stderr) {
-
-        __.assertThat(stdout, __.containsString('No file template nor folder destination given.'));
+        __.assertThat(stdout,
+          __.containsString('No file template nor folder destination given.'));
         done(error);
       });
     });
 
     lab.test('with generate command and invalid template file but no destination folder will display error message', function(done) {
-
       exec(cliEntryFile + ' generate //', function(error, stdout, stderr) {
-
-        __.assertThat(stdout, __.containsString('No folder destination given in second command input.'));
+        __.assertThat(stdout,
+          __.containsString('No folder destination given in second command input.'));
         done(error);
       });
     });
 
     lab.test('with generate command and invalid template file but valid destination folder will display error message', function(done) {
-
       exec(cliEntryFile + ' generate /zzz ../demo', function(error, stdout, stderr) {
-
-        __.assertThat(stdout, __.containsString('Not a valid file. Need a plain text file format in the first'));
+        __.assertThat(stdout,
+          __.containsString('Not a valid file. Need a plain text file format in the first'));
         done(error);
       });
     });
 
     lab.test('with generate command and invalid template file and invalid destination folder will display error message', function(done) {
-
       exec(cliEntryFile + ' generate /zzz ../adsf', function(error, stdout, stderr) {
-
-        __.assertThat(stdout, __.containsString('Not a valid file. Need a plain text file format in the first'));
+        __.assertThat(stdout,
+          __.containsString('Not a valid file. Need a plain text file format in the first'));
         done(error);
       });
     });
 
     lab.test('with generate command and valid template file but no destination folder will display error message', function(done) {
-
       exec(cliEntryFile + ' generate ../demo/example.txt', function(error, stdout, stderr) {
-        __.assertThat(stdout, __.containsString('No folder destination given in second command input.'));
+        __.assertThat(stdout,
+          __.containsString('No folder destination given in second command input.'));
         done(error);
       });
     });
 
     lab.test('with generate command and valid template file but invalid destination folder will display error message', function(done) {
-
       exec(cliEntryFile + ' generate ' + __dirname + '/../demo/example.txt ../adscd', function(error, stdout, stderr) {
-        __.assertThat(stdout, __.containsString('Not a valid folder.'));
+        __.assertThat(stdout,
+          __.containsString('Not a valid folder.'));
         done(error);
       });
     });
-
-
 
   });
 
@@ -123,48 +112,39 @@ lab.experiment('Cli commands when input is "dirgen" and', function() {
   lab.experiment('and with demo and information commands', function() {
 
     lab.test('with help command will display the help message', function(done) {
-
       exec(cliEntryFile + ' help', function(error, stdout, stderr) {
-
         __.assertThat(stdout, __.containsString('Description'));
         done(error);
       });
     });
 
     lab.test('with help alias command, "h", will display the help message', function(done) {
-
       exec(cliEntryFile + ' h', function(error, stdout, stderr) {
-
         __.assertThat(stdout, __.containsString('Description'));
         done(error);
       });
     });
 
     lab.test('with help alias option , "--help" will display the help message', function(done) {
-
       exec(cliEntryFile + ' --help', function(error, stdout, stderr) {
-
         __.assertThat(stdout, __.containsString('Description'));
         done(error);
       });
     });
 
     lab.test('with help alias option , "-h" will display the help message', function(done) {
-
       exec(cliEntryFile + ' -h', function(error, stdout, stderr) {
-
         __.assertThat(stdout, __.containsString('Description'));
         done(error);
       });
     });
 
     lab.test('with version command produces the same version number as in the package.json file', function(done) {
-
       exec(cliEntryFile + ' version', function(error, stdout, stderr) {
 
-        var packageJsonPath = path.resolve(__dirname, '../package.json');
-
-        var versionNumber = require(packageJsonPath).version.replace(/version /i, '');
+        var packageJsonPath = path.resolve(__dirname, '../package.json'),
+          versionNumber = require(packageJsonPath).version
+                            .replace(/version /i, '');
 
         __.assertThat(stdout, __.containsString(versionNumber));
         done(error);
@@ -172,43 +152,34 @@ lab.experiment('Cli commands when input is "dirgen" and', function() {
     });
 
     lab.test('with version command will display the module information message', function(done) {
-
       exec(cliEntryFile + ' version', function(error, stdout, stderr) {
-
         __.assertThat(stdout, __.containsString('Dirgen v'));
         done(error);
       });
     });
 
     lab.test('with version alias command, "v", will display the module information message', function(done) {
-
       exec(cliEntryFile + ' v', function(error, stdout, stderr) {
-
         __.assertThat(stdout, __.containsString('Dirgen v'));
         done(error);
       });
     });
 
     lab.test('with version alias option , "--version" will display the module information message', function(done) {
-
       exec(cliEntryFile + ' --version', function(error, stdout, stderr) {
-
         __.assertThat(stdout, __.containsString('Dirgen v'));
         done(error);
       });
     });
 
     lab.test('with version alias option , "-v" will display the module information message', function(done) {
-
       exec(cliEntryFile + ' -v', function(error, stdout, stderr) {
-
         __.assertThat(stdout, __.containsString('Dirgen v'));
         done(error);
       });
     });
 
     lab.test('with "demo" command will create the example folder', function(done) {
-
       exec(cliEntryFile + ' demo', function(error, stdout, stderr) {
 
         fs.isDirectoryAsync(__dirname + '/../demo/example-output').then(function(resolve, error) {
