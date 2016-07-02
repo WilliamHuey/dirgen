8623cbd1521d9c31eea1b8e1a5b67ed1a10d63ae
WilliamHuey <lliw1236@yahoo.com>
2016-07-02 13:12:02 -0400

    Switch out for loop with foreach loop

--- src/print-validations.js
+++ src/print-validations.js
@@ -1,6 +1,5 @@
 export default (messageOut, messageType, messages, messageCount) => {
-  let messagesKeyCount = messages.length;
-  for (let i = 0; i < messagesKeyCount; i++) {
-    messageOut[messageType](messages[i].message);
-  }
+  messages.forEach((message) => {
+    messageOut[messageType](message.message);
+  });
 };
