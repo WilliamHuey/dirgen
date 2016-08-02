b334ea19ae2633108f9e8f4290fd184eb54fc438
WilliamHuey <lliw1236@yahoo.com>
2016-08-02 00:22:57 -0400

    Outline the test cases for silent messages

---
+++ tests/display-messages.js
@@ -0,0 +1,30 @@
+module.exports = function(__, lab, cliEntryFile, exec) {
+  lab.experiment('and for display messages', function() {
+
+    lab.test('with warning messages and no silent flag will show no console warnings', function(done) {
+      done();
+    });
+
+    lab.test('with warning messages and with silent flag will show console warnings', function(done) {
+      done();
+    });
+
+    lab.test('with error messages and no silent flag will show no console warnings', function(done) {
+      done();
+    });
+
+    lab.test('with error messages and with silent flag will show console warnings', function(done) {
+      done();
+    });
+
+    lab.test('with warning and error messages and no silent flag will show no console warnings', function(done) {
+      done();
+    });
+
+    lab.test('with warning and error messages and with silent flag will show console warnings', function(done) {
+      done();
+    });
+
+
+  });
+};
