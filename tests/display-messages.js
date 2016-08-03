93ad989383c16791d665c2ecd0dd5e24e17a0138
WilliamHuey <lliw1236@yahoo.com>
2016-08-03 00:09:12 -0400

    Add error case silent flag test cases, Update eslint

--- tests/display-messages.js
+++ tests/display-messages.js
@@ -34,11 +34,30 @@ module.exports = function(__, lab, cliEntryFile, exec, fs) {
     });
 
     lab.test('with error messages and no silent flag will show console warnings', function(done) {
-      done();
+      fs.mkdirAsync(__dirname + '/case-outputs/errors-no-silent-flag')
+      .then(function() {
+
+        exec(cliEntryFile + ' g ' +
+        'tests/fixtures/errors-no-silent-flag.txt ' +
+        ' tests/case-outputs/errors-no-silent-flag', function(error, stdout, stderr) {
+            __.assertThat(stdout, __.containsString('Error:'));
+          done();
+        });
+      });
     });
 
     lab.test('with error messages and with silent flag will not show console warnings', function(done) {
-      done();
+      fs.mkdirAsync(__dirname + '/case-outputs/errors-with-silent-flag')
+      .then(function() {
+
+        exec(cliEntryFile + ' g ' +
+        'tests/fixtures/errors-with-silent-flag.txt ' +
+        ' tests/case-outputs/errors-with-silent-flag -s', function(error, stdout, stderr) {
+            __.assertThat(stdout, __.not(__.containsString('Error:')));
+          done();
+        });
+      });
+
     });
 
     lab.test('with warning and error messages and no silent flag will show console warnings', function(done) {
